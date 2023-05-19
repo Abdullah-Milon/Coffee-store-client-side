337f28caa644abbcd780d0c1bd2d813ed1bbcb43
Abdullah-Milon <almilonabdullah80@gmail.com>
2023-05-19 12:10:37 +0600

    sending data to backend server and also read it successfully

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,10 +1,47 @@
-
+import Swal from 'sweetalert2'
 
 const AddCoffee = () => {
+    const handleCoffee = event =>{
+        event.preventDefault();
+        const form = event.target;
+
+        const name = form.name.value;
+        const quantity = form.quantity.value;
+        const supplier = form.supplier.value;
+        const taste = form.taste.value;
+        const category = form.category.value;
+        const details = form.details.value;
+        const photo = form.photo.value;
+
+        const newCoffee = {name, quantity, supplier, taste, category, details, photo}
+        console.log(newCoffee);
+
+        // send data to the server
+        fetch('http://localhost:5000/coffee',{
+            method: 'POST',
+            headers:{
+                'content-type' : 'Application/json'
+            },
+            body:JSON.stringify(newCoffee)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+            if(data.insertedId){
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'User Added Successfully',
+                    icon: 'success',
+                    confirmButtonText: 'Cool'
+                  })
+            }
+        })
+
+    }
     return (
         <div className="bg-[#F4F3F0] p-28">
             <h1 className="text-3xl font-extrabold">Add a coffee</h1>
-            <form className="">
+            <form onSubmit={handleCoffee}>
                 {/* form name and quantity row */}
                 <div className="md:flex mb-4 ">
                     <div className="form-control md:w-1/2">
